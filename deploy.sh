4af1e1d620e1e1d9523d74764570f415166419fe
Horacio Jahir Gómez Gómez <86177245+JahirGomez@users.noreply.github.com>
2023-02-23 00:07:05 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 # !/usr/bin/bash
 
 sudo systemctl stop nginx
-#Comando para apagar ngrok
 
 cd ./Presentation_Page
 git pull
 
 sudo systemctl start nginx
-#Comando para encender ngrok
 
 ngrok http 192.168.56.101
